7c4a8320052c973a58b99f0ea104f695718dadfe
Mathieu Martin <webmat@gmail.com>
2009-02-11 11:13:00 -0800

    Add comment to help understand the meaning of aaet

--- lib/timeline_fu/acts_as_event_target.rb
+++ lib/timeline_fu/acts_as_event_target.rb
@@ -5,6 +5,8 @@ module TimelineFu
     end
 
     module ClassMethods
+      # Who's this timeline for? User? Person?
+      # Go and put acts_as_event_target in the model's class definition. 
       def acts_as_event_target
         has_many :timeline_events, :as => :target
       end
