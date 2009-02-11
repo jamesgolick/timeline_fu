9cecbdb6b0569d8657614324a00649d8fecc9b7a
Mathieu Martin <webmat@gmail.com>
2009-02-11 12:11:31 -0800

    Fix tests that fail because we convert the event_type to a string...
    Note that previous to adding that to_s, the YAML representation of a symbol would have been saved to the db.

--- test/fires_test.rb
+++ test/fires_test.rb
@@ -8,31 +8,31 @@ class FiresTest < Test::Unit::TestCase
   
   def test_should_fire_the_appropriate_callback
     @list = List.new(hash_for_list(:author => @james));
-    TimelineEvent.expects(:create!).with(:actor => @james, :target => @list, :secondary_target => @list, :event_type => :list_created)
+    TimelineEvent.expects(:create!).with(:actor => @james, :target => @list, :secondary_target => @list, :event_type => 'list_created')
     @list.save
   end
   
   def test_should_only_fire_if_the_condition_evaluates_to_true
-    TimelineEvent.expects(:create!).with(:actor => @mat, :target => @james, :event_type => :follow_created)
+    TimelineEvent.expects(:create!).with(:actor => @mat, :target => @james, :event_type => 'follow_created')
     @james.new_watcher = @mat
     @james.save
   end
   
   def test_should_not_fire_if_the_if_condition_evaluates_to_false
-    TimelineEvent.expects(:create!).with(:actor => @mat, :target => @james, :event_type => :follow_created).times(0)
+    TimelineEvent.expects(:create!).with(:actor => @mat, :target => @james, :event_type => 'follow_created').times(0)
     @james.new_watcher = nil
     @james.save
   end
   
   def test_should_fire_event_with_symbol_based_if_condition_that_is_true
     @james.fire = true
-    TimelineEvent.expects(:create!).with(:actor => @james, :target => @james, :event_type => :person_updated)
+    TimelineEvent.expects(:create!).with(:actor => @james, :target => @james, :event_type => 'person_updated')
     @james.save
   end
   
   def test_should_fire_event_with_symbol_based_if_condition
     @james.fire = false
-    TimelineEvent.expects(:create!).with(:actor => @james, :target => @james, :event_type => :person_updated).times(0)
+    TimelineEvent.expects(:create!).with(:actor => @james, :target => @james, :event_type => 'person_updated').times(0)
     @james.save
   end
 end
