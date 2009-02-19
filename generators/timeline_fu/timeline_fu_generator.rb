85c235081e384da38549906423d65c141312da03
Mathieu Martin <webmat@gmail.com>
2009-02-19 17:16:09 -0500

    Frickin' renames

--- generators/timeline_fu/timeline_fu_generator.rb
+++ generators/timeline_fu/timeline_fu_generator.rb
@@ -1,4 +1,4 @@
-class TimelineEventGenerator < Rails::Generator::Base
+class TimelineFuGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.migration_template 'migration.rb', 'db/migrate', 
