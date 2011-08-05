109e58cf73fadf63ab85d175f71178299373e15b
Daniel Amireh <daniel@amireh.org>
2011-08-05 23:58:50 +0300

    added indices to timeline_events migration

--- generators/timeline_fu/templates/migration.rb
+++ generators/timeline_fu/templates/migration.rb
@@ -5,11 +5,13 @@ class CreateTimelineEvents < ActiveRecord::Migration
       t.integer               :subject_id,    :actor_id,    :secondary_subject_id
       t.timestamps
     end
+
+    add_index :timeline_events, [:subject_id , :subject_type]
+    add_index :timeline_events, [:actor_id , :actor_type]
+    add_index :timeline_events, [:secondary_subject_id , :secondary_subject_type], :name => 'secondary_subject_timeline_events'
   end
- 
+
   def self.down
     drop_table :timeline_events
   end
 end
- 
-
