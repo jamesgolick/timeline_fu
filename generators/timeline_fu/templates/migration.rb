29ac0a1fc8dbce489ccb0fac58d66b715cba6f42
Mikhail S. Pobolovets <styx.mp@gmail.com>
2012-07-18 16:58:15 +0300

    Updated migration syntax.
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- generators/timeline_fu/templates/migration.rb
+++ generators/timeline_fu/templates/migration.rb
@@ -1,15 +1,13 @@
 class CreateTimelineEvents < ActiveRecord::Migration
-  def self.up
+  def up
     create_table :timeline_events do |t|
       t.string   :event_type, :subject_type,  :actor_type,  :secondary_subject_type
       t.integer               :subject_id,    :actor_id,    :secondary_subject_id
       t.timestamps
     end
   end
- 
-  def self.down
+
+  def down
     drop_table :timeline_events
   end
 end
- 
-
