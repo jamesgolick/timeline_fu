a2ee5a52dea59354bd1b9a7243fc43ce6cb081cf
Mikhail S. Pobolovets <styx.mp@gmail.com>
2012-08-09 11:32:03 +0300

    Added dependency status jaw.
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- generators/timeline_fu/templates/model.rb
+++ generators/timeline_fu/templates/model.rb
@@ -1,4 +1,6 @@
 class TimelineEvent < ActiveRecord::Base
+  attr_accessible :event, :actor, :subject, :secondary_subject
+
   belongs_to :actor,              polymorphic: true
   belongs_to :subject,            polymorphic: true
   belongs_to :secondary_subject,  polymorphic: true
